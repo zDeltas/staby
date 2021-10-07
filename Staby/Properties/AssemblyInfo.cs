db7b843accd340d3ddac81bee27586f1bee2b572
Damien LE BORGNE <damien.leborgne@epsi.fr>
2021-10-07 15:52:05 +0200

    ico

--- Staby/Properties/AssemblyInfo.cs
+++ Staby/Properties/AssemblyInfo.cs
@@ -20,8 +20,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("38ab6e8c-1895-4c7e-a77b-ba03ae1cfb95")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -32,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("fr-FR")]
 
